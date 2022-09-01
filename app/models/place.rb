543fba6bfa5fb4f4790bbe8b200749badace487f
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-09-01 21:08:59 +0900

    place モデルの作成

---
+++ app/models/place.rb
@@ -0,0 +1,11 @@
+class Place < ApplicationRecord
+
+  validates :title, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
+  belongs_to :user
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
+
+end
