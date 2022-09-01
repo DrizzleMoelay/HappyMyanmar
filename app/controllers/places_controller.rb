0dd84b3c9e3c4287ca1c3476e6ab9f860edde058
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-09-01 22:06:49 +0900

    詳細ページ実装

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
   def index
+    @places = Place.all
   end
 
   def new
@@ -15,9 +19,46 @@ class PlacesController < ApplicationController
     end
   end
 
+  def show
+    @place = Place.find(params[:id])
+    @comment = Comment.new
+    @comments = @place.comments.includes(:user)
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+    if @place.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+     if @place.save
+      redirect_to place_path 
+    else
+      @place.attributes = (place_params)
+      render :edit
+    end
+  end
+
+  def destroy
+    place = Place.find(params[:id])
+    place.destroy
+    redirect_to root_path
+  end
+
+
   private
 
   def place_params
     params.require(:place).permit(:title, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
